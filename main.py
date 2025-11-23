620d8971220ffc183c7fdfd898a02cfc74c7d07c
Jack Toumey <jack@toumey.io>
2025-11-22 17:55:06 -0700

    loading and splitting text

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from facts!")
+"""script for running fact file through langchain"""
 
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
 
-if __name__ == "__main__":
-    main()
+text_splitter = CharacterTextSplitter(
+  separator="\n",
+  chunk_size=200,
+  chunk_overlap=100,
+)
+
+loader = TextLoader("facts.txt")
+docs = loader.load_and_split(
+  text_splitter,
+)
+
+for doc in docs:
+  print(doc.page_content)
+  print("\n")
