695fd99370b9345bf28f8963f309bae36ea2646f
Jack Toumey <jack@toumey.io>
2025-12-23 08:04:39 -0700

    add chroma

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
+
+embeddings = OpenAIEmbeddings()
 
 text_splitter = CharacterTextSplitter(
   separator="\n",
@@ -14,6 +18,22 @@ docs = loader.load_and_split(
   text_splitter,
 )
 
-for doc in docs:
-  print(doc.page_content)
+db = Chroma.from_documents(
+  docs,
+  embedding=embeddings,
+  persist_directory="emb",
+)
+
+results = db.similarity_search_with_score(
+  "What is an interesting fact about the english language?",
+  k=1,
+)
+
+# for doc in docs:
+#   print(doc.page_content)
+#   print("\n")
+
+for result in results:
   print("\n")
+  print(result[1])
+  print(result[0].page_content)
